32f7b04b251e2d68c246567d070f562c8cf5208a
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-10 22:33:53 +0300

    state creations

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Defines the Amenity class."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship 
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """
     Represents class State.
     Attributes:
@@ -16,4 +16,4 @@ class Amenity(BaseModel):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
-                                    viewonly=False)
+                                    viewonly=True)
--- models/place.py
+++ models/place.py
@@ -15,13 +15,13 @@ from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
