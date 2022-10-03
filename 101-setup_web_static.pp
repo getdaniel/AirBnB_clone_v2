0332fd52c3ba6246b6b66075f94e035f49d6a2f8
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-03 21:13:20 +0300

    fixing the code

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,6 @@
 # Configures a web server for deployment of web_static.
 # Nginx configuration file
 
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
